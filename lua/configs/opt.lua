4b2186549eb24c7d347b0863bd33341dd4433a3e
Sylfrax <3532729396@qq.com>
2026-07-28 00:48:42 +0000

    add change

--- lua/configs/opt.lua
+++ lua/configs/opt.lua
@@ -1,7 +1,7 @@
 vim.opt.number = true
 vim.opt.termguicolors = true
 vim.opt.mouse = 'a'
-vim.opt.cmdheight = 0
+vim.opt.cmdheight = 2
 vim.opt.tabstop = 4
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
